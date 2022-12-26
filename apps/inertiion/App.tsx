ae1ef841a67d782db76fd2f40ba52f149ecbe670
thankyouthatscool <85291297+thankyouthatscool@users.noreply.github.com>
2022-12-26 19:01:36 +1000

    Remove and start again.
    Something happened with the packages, and could not use navigation.

--- apps/inertiion/App.tsx
+++ apps/inertiion/App.tsx
@@ -1,13 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-
-import { AppRoot } from "@components";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-      <AppRoot />
-    </SafeAreaProvider>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- apps/inertiion/babel.config.js
+++ apps/inertiion/babel.config.js
@@ -1,23 +1,6 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      [
-        "module-resolver",
-        {
-          alias: {
-            "@": "./",
-            "@components": "./components",
-            "@hooks": "./hooks",
-            "@screens": "./screens",
-            "@store": "./store",
-            "@theme": "./theme",
-            "@types": "./types",
-            "@utils": "./utils",
-          },
-        },
-      ],
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
